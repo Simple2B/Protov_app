c36bb01ed22d0e938e77ed18a6d0d09318cdfc04
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-05-13 04:20:42 +0300

    addSubmit

--- src/components/Add/AddSubmit.tsx
+++ src/components/Add/AddSubmit.tsx
@@ -10,12 +10,14 @@ export default function AddSubmit(): ReactElement {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (location.responseData.add_object_success) {
+    if (location.responseData.message.add_object_success) {
       setStatus("Success!");
     } else {
       setStatus("Fail!");
     }
   }, []);
+
+  // location.responseData.message
 
   const handleHome = () => {
     navigate("/");
@@ -30,21 +32,26 @@ export default function AddSubmit(): ReactElement {
       </div>
       <h1 className="add_submit-title">Add</h1>
       <div className="add_submit-info">
-        {location.data.artist_firstname} {location.artist_surname},{" "}
+        {location.data.artist_firstname} {location.data.artist_surname},{" "}
         {location.data.title}, {location.data.year}
       </div>
       <div className="add_submit-id">artist ID: {location.data.artist_id}</div>
+      <div className="add_submit-id"><strong>object:</strong> {location.responseData.message.object}</div>
+      <div className="add_submit-id"><strong>object ID:</strong> {location.responseData.message.id_object}</div>
+
       <h2 className="verify_methods-title">Verification methods:</h2>
       <div>
+        {location.responseData.message.methods1 ? <> <strong>methods1:</strong> {location.responseData.message.methods1}</> : null}
         <>
-          {Object.values(location.data.methods)[0] &&
-            Object.keys(location.data.methods)[0]}
+          {/* {Object.values(location.data.methods)[0] &&
+            Object.keys(location.data.methods)[0]} */}
         </>
       </div>
       <div>
+          {location.responseData.message.methods2 ? <> <strong>methods2:</strong> {location.responseData.message.methods2}</> : null}
         <>
-          {Object.values(location.data.methods)[1] &&
-            Object.keys(location.data.methods)[1]}
+          {/* {Object.values(location.data.methods)[1] &&
+            Object.keys(location.data.methods)[1]} */}
         </>
       </div>
       <div
